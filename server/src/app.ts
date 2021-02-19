0153f3106eee2c6abc13e6ddc310e16c9207ae72
sochacki111 <michalsochacki111@gmail.com>
2021-02-19 08:27:57 +0100

    Basic file setup

---
+++ server/src/app.ts
@@ -0,0 +1,38 @@
+import express, { Application } from 'express';
+import bodyParser from 'body-parser';
+import passport from 'passport';
+import cors from 'cors';
+import morgan from 'morgan';
+
+// Create a new express app instance
+const app: Application = express();
+
+// Middlewares
+app.use(cors());
+app.use(passport.initialize());
+app.use(morgan('dev'));
+
+// Passport configuration
+// passport.use(JwtStrategy);
+
+// Express configuration
+declare global {
+  namespace Express {
+    interface User {
+      _id: string;
+      email: string;
+      swapiHeroId: string;
+    }
+  }
+}
+
+app.set('port', process.env.PORT || 8080);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Swagger
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+export default app;
