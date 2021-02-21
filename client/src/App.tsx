1aff4ca5d44c27dfdf18a861807897e874c4d9d9
sochacki111 <michalsochacki111@gmail.com>
2021-02-21 11:52:42 +0100

    apply login and register screens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import ProPlayers from './containers/ProPlayers';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import RegisterScreen from './components/RegisterScreen';
+import SigninScreen from './components/SigninScreen';
 
 function App() {
   return (
     <div className="App">
       <ProPlayers />
+      <Route path="/signin" component={SigninScreen}></Route>
+      <Route path="/register" component={RegisterScreen}></Route>
     </div>
   );
 }
