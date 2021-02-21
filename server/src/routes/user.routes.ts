30ca0c712de52b139adda8cca46f6401bc924065
sochacki111 <michalsochacki111@gmail.com>
2021-02-21 12:08:50 +0100

    fix http verbs

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -3,8 +3,8 @@ import UsersController from '../controllers/users.controller';
 
 const router = Router();
 
-router.get('/new', UsersController.register);
-router.get('/login', UsersController.login);
+router.post('/new', UsersController.register);
+router.post('/login', UsersController.login);
 router.get('/fetch_data', UsersController.fetchProPlayers);
 
 export default router;
